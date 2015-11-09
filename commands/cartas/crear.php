938f3e30e92402bcdffcf94470847dff44f3fcf6
pilochi1989 <josecarlos89@gmail.com>
2015-11-08 21:27:53 -0500

    - Se agrega carta de despacho de endosos

--- commands/cartas/crear.php
+++ commands/cartas/crear.php
@@ -10,6 +10,38 @@ class crear extends sessionCommand{
 			}else if($poliza->getIdRamo()=="4"){
 				$this->addBlock("amed");
 			}
+			//inicio - despacho de endosos
+			if($this->request->idPoliza){
+				$this->addVar("idPoliza",$this->request->idPoliza);
+				$poliza = Fabrica::getFromDB("Poliza",$this->request->idPoliza);
+				//primero busco los datos de cobro de la poliza madre
+				$endosos = Fabrica::getAllFromDB("Endoso", array("idPoliza = " . $this->request->idPoliza));
+				$listaEndosos = array();
+				$i = 0;
+				if(count($endosos)){
+					$this->addBlock("bloqueResultados");	
+				}else{
+					$this->addBlock("bloqueNoResultados");				
+				}
+				foreach($endosos as $endoso){
+					$cobro = Fabrica::getFromDB("Cobro",$endoso->getIdCobro());
+					$listaEndosos[$i]["idLista"] = $i + 1;
+					$listaEndosos[$i]["documento"] = $endoso->getDocumento();
+					$listaEndosos[$i]["avisoCobranza"] = $cobro->getAvisoDeCobranza();
+					$listaEndosos[$i]["primaNeta"] = number_format($cobro->getPrimaNeta(),2);
+					$listaEndosos[$i]["comision"] = number_format($cobro->getComision(),2);;
+					$listaEndosos[$i]["idEndoso"] = $endoso->getId();
+					
+					if($cobro->getIdLiquidacion() == ""){
+						$listaEndosos[$i]["estadoCobranza"] = '<i class="icon-circle red"></i>&nbsp;&nbsp;&nbsp;Pendiente';
+					}else{
+						$listaEndosos[$i]["estadoCobranza"] = '<i class="icon-circle green"></i>&nbsp;&nbsp;&nbsp;<a href="?do=liquidaciones.ver&idLiquidacion=' . $cobro->getIdLiquidacion() . '">Liquidada</a>';
+					}
+					$i++;
+				}
+				$this->addLoop("endosos", $listaEndosos);
+			}
+			//fin - despacho de endosos
 			$this->addVar("idPoliza",$this->request->idPoliza);
 			$this->processTemplate("cartas/crear.html");
 		}
---
+++ commands/cartas/despachoEndoso.php
@@ -0,0 +1,97 @@
+<?php
+class despachoEndoso extends sessionCommand{
+	function execute(){
+		$fc=FrontController::instance();
+		$usuario=$this->getUsuario();
+		if($this->request->idPoliza){
+			if($this->request->idEndoso){
+				//nueva carta
+				setlocale(LC_ALL,"es_ES");
+				$this->addVar("idPoliza", $this->request->idPoliza);
+				$this->addBlock("poliza");
+				$this->addVar("fecha", strftime("%A %d de %B del %Y"));
+				$poliza = Fabrica::getFromDB("Poliza",$this->request->idPoliza);
+				$endoso = Fabrica::getFromDB("Endoso",$this->request->idEndoso);
+				$cobro = Fabrica::getFromDB("Cobro",$endoso->getIdCobro());
+				//print_r($cobro);
+				$simbolo="US$ ";
+				if($cobro->getMoneda()=="Dolares"){
+					$simbolo="US$ ";
+				}elseif($cobro->getMoneda()=="Soles"){
+					$simbolo="S/. ";
+				}elseif($poliza->getMoneda()=="Euros"){
+					$simbolo="&euro; ";
+				}
+				$distritos = array(
+					"99" => "Sin Definir",
+					"98" => "Provincia",
+					"1" => "Lima",
+					"2" => "Ancón",
+					"3" => "Ate",
+					"4" => "Barranco",
+					"5" => "Breña",
+					"6" => "Carabayllo",
+					"7" => "Chaclacayo",
+					"8" => "Chorrillos",
+					"40" => "Cieneguilla",
+					"7" => "Comas",
+					"10" => "El Agustino",
+					"28" => "Independencia",
+					"11" => "Jesús María",
+					"12" => "La Molina",
+					"13" => "La Victoria",
+					"14" => "Lince",
+					"39" => "Los Olivos",
+					"15" => "Lurigancho-Chosica",
+					"16" => "Lurin",
+					"17" => "Magdalena del Mar",
+					"21" => "Pueblo Libre",
+					"18" => "Miraflores",
+					"19" => "Pachacámac",
+					"20" => "Pucusana",
+					"22" => "Puente Piedra",
+					"24" => "Punta Hermosa",
+					"23" => "Punta Negra",
+					"25" => "Rímac",
+					"26" => "San Bartolo",
+					"41" => "San Borja",
+					"27" => "San Isidro",
+					"36" => "San Juan de Lurigancho",
+					"29" => "San Juan de Miraflores",
+					"30" => "San Luis",
+					"31" => "San Martín de Porres",
+					"32" => "San Miguel",
+					"43" => "Santa Anita",
+					"37" => "Santa María del Mar",
+					"38" => "Santa Rosa",
+					"33" => "Santiago de Surco",
+					"34" => "Surquillo",
+					"42" => "Villa El Salvador",
+					"35" => "Villa María del Triunfo"
+				);
+				$this->addVar("inicio", $poliza->getInicioVigencia("DATE"));
+				$this->addVar("pol", $poliza->getNumeroPoliza());
+				$this->addVar("fin	", $poliza->getFinVigencia("DATE"));
+				$compania = Fabrica::getFromDB("Compania", $poliza->getIdCompania());
+				$this->addVar("cia", $compania->getSigla());
+				$cliente = Fabrica::getFromDB("Cliente", $poliza->getIdCliente());
+				$this->addVar("nombre", $cliente->getNombre());
+				if($cliente->getDireccion() == ""){
+					$this->addVar("direccion", "[direccion]");
+				}else{
+					$this->addVar("direccion", $cliente->getDireccion());
+				}
+				$this->addVar("distrito", $distritos[$cliente->getDistrito()]);
+				$ramo = Fabrica::getFromDB("Ramo", $poliza->getIdRamo());
+				$this->addVar("ramo", $ramo->getNombre());
+				$this->addVar("idPoliza",$this->request->idPoliza);
+				$this->addVar("aviso", $cobro->getAvisoDeCobranza());
+				$this->addVar("moneda", $simbolo);
+				$this->addVar("monto", number_format($cobro->getTotalFactura(),2));
+				$this->addLayout("carta");
+				$this->processTemplate("cartas/despachoEndoso.html");
+			}
+		}
+	}
+}
+?>
