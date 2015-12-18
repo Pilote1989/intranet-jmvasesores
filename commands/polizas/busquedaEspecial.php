ac6e1a2bdd4de38385e4f37184dab4c164b0636d
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2015-12-18 00:10:04 +0000

    - Se corrige la busqueda especial cuando el aviso de cobranza es de un endoso

--- commands/polizas/busquedaEspecial.php
+++ commands/polizas/busquedaEspecial.php
@@ -1,6 +1,6 @@
 <?php
 class busquedaEspecial extends sessionCommand{
-	function execute(){
+	function execute(){	
 		$fc=FrontController::instance();
 		$usuario=$this->getUsuario();
 		$this->addVar("doFalso", $this->request->do);
@@ -12,11 +12,27 @@ class busquedaEspecial extends sessionCommand{
 			$response["cuenta"]=count($cobro);
 			if(count($cobro)=="1"){
 				$poliza=Fabrica::getAllFromDB("Poliza",array("idCobro = '" . $cobro[0]->getId() . "'"));
-				$response["respuesta"]="SUCCESS";
-				$response["id"]=$poliza[0]->getId();
-				$response["m"]="Se encontro una poliza, redireccionando...";
+				if(count($poliza)=="1"){
+					//Se encontro una poliza
+					$response["respuesta"]="SUCCESS";
+					$response["id"]=$poliza[0]->getId();
+					$response["m"]="Se encontro una poliza, redireccionando...";
+				}else{
+					//Debe ser un endoso
+					$endoso=Fabrica::getAllFromDB("Endoso",array("idCobro = '" . $cobro[0]->getId() . "'"));
+					if(count($endoso)=="1"){
+						//Se encontro un endoso
+						$response["respuesta"]="SUCCESS";
+						$response["id"]=$endoso[0]->getIdPoliza();
+						$response["m"]="Se encontro un endoso, redireccionando a la poliza...";
+					}else{
+						//Error
+						$response["m"]="Error desconocido.";
+						
+					}
+				}
 			}elseif(count($cobro)=="0"){
-				$response["m"]="No se encontro ninguna poliza con ese Aviso de Cobranza";
+				$response["m"]="No se encontro ninguna poliza con ese Aviso de Cobranza.";
 			}else{
 				$response["m"]="Se encontro mas de 1 poliza con ese Aviso de Cobranza.";
 			}
