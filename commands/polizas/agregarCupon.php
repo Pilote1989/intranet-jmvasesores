d4183a59d2f149b252dbe41699ed17c194066942
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2017-07-31 17:49:07 +0000

    - Correccion en la edicion de cupones

--- commands/polizas/agregarCupon.php
+++ commands/polizas/agregarCupon.php
@@ -4,6 +4,19 @@ class agregarCupon extends sessionCommand{
 		$fc=FrontController::instance();
 		$usuario=$this->getUsuario();
 		$this->addVar("idPoliza",$this->request->idPoliza);
+		if($this->request->idCupon){
+			$this->addVar("idCupon",$this->request->idCupon);
+			$cupon = Fabrica::getFromDB("Cupon",$this->request->idCupon);
+			$this->addVar("monto",$cupon->getMonto());	
+			$this->addVar("fechaVencimiento",$cupon->getFechaVencimiento("DATE"));	
+			$this->addVar("numeroCupon",$cupon->getNumeroCupon());
+		}else{
+			$this->addEmptyVar("idCupon");
+			$this->addEmptyVar("monto");	
+			$this->addEmptyVar("fechaVencimiento");	
+			$this->addEmptyVar("numeroCupon");
+		}
+		
 		$this->processTemplate("polizas/agregarCupon.html");
 		
 	}
