33e1b4e19c6a2328b39e9b12cd99bb6146aca60f
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2017-09-12 00:46:31 +0000

    - Se corrige el menu de navegacion

--- commands/menus/menuNavegacion.php
+++ commands/menus/menuNavegacion.php
@@ -5,27 +5,6 @@ class menuNavegacion extends SessionCommand{
 		$fc=FrontController::instance();
 		$user = $this->getUsuario();
 		$this->addEmptyVar("places");
-		/*
-		$this->addEmptyVar("inicio");
-		$this->addEmptyVar("administracion");
-		$this->addEmptyVar("personas.inscribirUsuario");
-		$this->addEmptyVar("personas.verPersonas");
-		$this->addEmptyVar("personas.cambiarClave");
-		$this->addEmptyVar("polizas");
-		$this->addEmptyVar("polizas.editarDatosBasicos");
-		$this->addEmptyVar("polizas.verPolizas");
-		$this->addEmptyVar("clientes");
-		$this->addEmptyVar("clientes.editarDatosBasicos");
-		$this->addEmptyVar("clientes.verClientes");
-		$this->addEmptyVar("companias");
-		$this->addEmptyVar("companias.editarDatosBasicos");
-		$this->addEmptyVar("companias.verCompanias");
-		$this->addEmptyVar("reportes");
-		$this->addEmptyVar("reportes.generar");
-		$this->addEmptyVar("reportes.historico");
-		$this->addEmptyVar("solicitudes");
-		$this->addEmptyVar("solicitudes.crearSolicitud");
-		$this->addEmptyVar("solicitudes.verSolicitudes");*/
 		if($this->request->doFalso == "personas.verPortada"){
 			$places = array("Portada");
 		}elseif($this->request->doFalso == "solicitudes.crearSolicitud"){
@@ -46,7 +25,7 @@ class menuNavegacion extends SessionCommand{
 			$places = array("Polizas", "Editar Polizas");
 		}elseif($this->request->doFalso == "polizas.verPolizas"){
 			$places = array("Polizas", "Ver Polizas");
-		}elseif($this->request->doFalso == "clientes.verDatosBasicos"){
+		}elseif($this->request->doFalso == "clientes.ver"){
 			$places = array("Clientes", "Ver Clientes");
 		}elseif($this->request->doFalso == "clientes.editarDatosBasicos"){
 			$places = array("Clientes", "Editar Clientes");
