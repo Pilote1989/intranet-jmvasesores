32d45dcb446cc4fe745b0ce45b8554808ba43cec
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2017-11-11 00:30:47 +0000

    - Se agregan campos de perfiles y se condiciona la comision en la personas

--- commands/personas/verDatosBasicos.php
+++ commands/personas/verDatosBasicos.php
@@ -14,10 +14,27 @@ class verDatosBasicos extends sessionCommand{
 			$this->addVar("correo", $persona->getMail());
 			$this->addVar("usuario", $persona->getUserName());
 			$this->addVar("comision", $persona->getComision());
+			if($persona->getVendedor()){
+				$this->addVar("vendedor", "Si");
+				$this->addBlock("vende");
+			}else{
+				$this->addVar("vendedor", "No");
+			}
 			if($persona->getHabilitado() == "1"){
 				$this->addVar("estado", '<i class="ace-icon fa fa-circle light-green middle"></i> Habilitado');
 			}else{
 				$this->addVar("estado", '<i class="ace-icon fa fa-circle red middle"></i> Deshabilitado');
+			}
+			$personaEnPerfil = Fabrica::getAllFromDB("PersonaEnPerfil",array("idPersona ='".$this->request->idPersona."'"));
+			$perfiles = array();
+			foreach($personaEnPerfil as $perfilP){
+				$perfil = Fabrica::getFromDB("Perfil",$perfilP->getIdPerfil());
+				$perfiles[] = $perfil->getNombre();
+			}
+			if(count($perfiles)){
+				$this->addVar("perfiles",implode(", ", $perfiles));
+			}else{
+				$this->addVar("perfiles","No tiene perfiles asociados.");
 			}
 			$this->addLayout("ace");
 			$this->processTemplate("personas/verDatosBasicos.html");
