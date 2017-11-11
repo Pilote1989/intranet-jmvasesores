cd55a5ac5488763af8b668921a4d346aa41c7dc2
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2017-11-11 00:31:53 +0000

    - Se agrega verificacion para evitar la duplicidad de numeros de polizas

---
+++ commands/polizas/revisarNumeroPoliza.php
@@ -0,0 +1,26 @@
+<?php
+class revisarNumeroPoliza extends sessionCommand{
+	function execute(){
+		header('Content-type: application/json');
+		$fc=FrontController::instance();
+		$usuario=$this->getUsuario();
+		$valid = false;
+		//if($this->request->numeroPoliza && $this->request->cia){
+			$polizas=Fabrica::getAllFromDB("Poliza",array("numeroPoliza = '" . $this->request->numeroPoliza . "'","idCompania = '" . $this->request->cia . "'"));
+			if(count($polizas)>0){
+				$valid = false;
+				if($this->request->poliza){
+					foreach($polizas as $poliza){
+						if($poliza->getId()==$this->request->poliza){
+							$valid = true;
+						}						
+					}
+				}
+			}else{
+				$valid = true;
+			}		
+		//}
+		echo json_encode($valid);
+	}
+}
+?>
