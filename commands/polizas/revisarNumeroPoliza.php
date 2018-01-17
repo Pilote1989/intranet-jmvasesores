c8fcfacbf4c8698ad7e3c305ccd1fef5e752b069
Jose Carlos Martinez Vargas <josecarlos89@gmail.com>
2018-01-17 16:21:29 +0000

    - Se corrige validacion de numero de poliza, no cuenta poliza eliminadas

--- commands/polizas/revisarNumeroPoliza.php
+++ commands/polizas/revisarNumeroPoliza.php
@@ -6,7 +6,7 @@ class revisarNumeroPoliza extends sessionCommand{
 		$usuario=$this->getUsuario();
 		$valid = false;
 		//if($this->request->numeroPoliza && $this->request->cia){
-			$polizas=Fabrica::getAllFromDB("Poliza",array("numeroPoliza = '" . $this->request->numeroPoliza . "'","idCompania = '" . $this->request->cia . "'"));
+			$polizas=Fabrica::getAllFromDB("Poliza",array("numeroPoliza = '" . $this->request->numeroPoliza . "'","idCompania = '" . $this->request->cia . "'", "estado = '1'"));
 			if(count($polizas)>0){
 				$valid = false;
 				if($this->request->poliza){
